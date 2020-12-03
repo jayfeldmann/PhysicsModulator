e578b3d36d20712428713201b1a6cd8a42f2bd9c
Jan Feldmann <janlukasfeldmann@gmail.com>
2020-12-03 17:00:54 +0100

    Added Working SeekTarget and AvoidWall funktionality for Boids

--- Bachelor-Unity/Assets/Scripts/Boid.cs
+++ Bachelor-Unity/Assets/Scripts/Boid.cs
@@ -22,16 +22,16 @@ public class Boid : MonoBehaviour
     
     public bool isSelected = false;
 
-    public Rigidbody2D rigidBody2d;
-
     public GameObject target;
 
     //Boid specific movement Variables
     private float mass = 1;
-    private Vector2 _velocity = new Vector2(0,0);
+    private Vector2 _velocity = new Vector2(70,70);
     private Vector2 _acceleration = new Vector2(0,0);
-    private float maxSpeed = 4;
-    private float maxForce = 0.1f;
+    private float maxSpeed = 70;
+    private float maxForce = 1f;
+    private int arriveRadius = 50;
+    private int boundryOffset = 10;
 
     private void Awake()
     {
@@ -41,18 +41,25 @@ public class Boid : MonoBehaviour
     private void MoveBoid()
     {
         Vector2 targetPos = target.transform.position;
-        TeleportToOtherSide();
-        Debug.DrawRay(transform.position,transform.up*8);
-
-        SeekTarget(targetPos);
+        //TeleportToOtherSide();
+        //Debug.DrawRay(transform.position,transform.up*8);
+        AvoidWalls();
+        //SeekTarget(targetPos);
         
         _velocity += _acceleration;
         _velocity = Vector2.ClampMagnitude(_velocity,maxSpeed);
         
+        LookAt2D(_velocity);
         transform.position +=  new Vector3(_velocity.x,_velocity.y,0) * Time.deltaTime;
 
         _acceleration *= 0;
 
+    }
+
+    private void LookAt2D(Vector2 target)
+    {
+        float rot_z = Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
     }
 
     private void ApplyForce(Vector2 force)
@@ -60,12 +67,64 @@ public class Boid : MonoBehaviour
         _acceleration += force;
     }
 
+    private void AvoidWalls()
+    {
+        var pos = transform.position;
+        Vector2 desired = new Vector2();
+        bool isWall = false;
+        if (pos.x >= _screenBounds.x - _objectWidth-boundryOffset)
+        {
+            desired = new Vector2(-maxSpeed,_velocity.y);
+            isWall = true;
+        } 
+        else if (pos.x <= _screenBounds.x *-1 + _objectWidth +boundryOffset)
+        {
+            desired = new Vector2(maxSpeed,_velocity.y);  
+            isWall = true;
+        }
+
+        if (pos.y >= _screenBounds.y - _objectHeight-boundryOffset)
+        {
+            desired = new Vector2(_velocity.x,-maxSpeed);
+            isWall = true;
+        }
+        else if (pos.y <= _screenBounds.y *-1 + _objectHeight+boundryOffset)
+        {
+            desired = new Vector2(_velocity.x,maxSpeed);
+            isWall = true;
+        }
+
+        if (isWall)
+        {
+            desired.Normalize();
+            desired *= maxSpeed;
+            var steer = desired-_velocity;
+            steer = Vector2.ClampMagnitude(steer,maxForce);
+            ApplyForce(steer);
+        }
+    }
+    private void WanderMovement()
+    {
+        
+    }
+
     private void SeekTarget(Vector3 targetPosition)
     {
         Vector2 desiredVelocity = targetPosition - transform.position;
+
+        float d = desiredVelocity.magnitude;
         desiredVelocity.Normalize();
-        desiredVelocity *= maxSpeed;
-        
+
+        if (d < arriveRadius)
+        {
+            float m = ExtensionMethods.Map(d, 0, arriveRadius, 0, maxSpeed);
+            desiredVelocity *= m;
+        }
+        else
+        {
+            desiredVelocity *= maxSpeed;
+        }
+
         Vector2 steer = desiredVelocity - _velocity;
         
         steer = Vector2.ClampMagnitude(steer, maxForce);
