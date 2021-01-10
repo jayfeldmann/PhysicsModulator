11f3e92529b81509f4505b00489858986c2a4f11
Jan Feldmann <janlukasfeldmann@gmail.com>
2021-01-10 17:15:55 +0100

    Commented Python script

--- PythonPlotting/plotUnityData.py
+++ PythonPlotting/plotUnityData.py
@@ -1,7 +1,12 @@
+# Dieses script dient als Hilfsscript, um die aus Unity gelesenen MIDI Daten
+# zu verwenden um Graphen zu generieren, die als Grafik in der Bachelorarbeit verwendet 
+# werden können.
+
 import matplotlib, json
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Funktion um aus den vorbereiteten Daten einen Graphen zu generieren und anzuzeigen.
 def PlotGraph(duration, step, dataValues):
     t = np.arange(0.0,duration,step)
     
@@ -13,6 +18,7 @@ def PlotGraph(duration, step, dataValues):
 
     plt.show()
 
+# Funktion um JSON Daten auszulesen und für die Generation vorzubereiten.
 def ReadUnityData(dataFilePath):
     jsonData = GetJsonData(dataFilePath)
     duration = float(jsonData["readDuration"])
@@ -21,11 +27,13 @@ def ReadUnityData(dataFilePath):
     values = np.array(jsonData["values"])
     PlotGraph(duration,timeStep,values)
 
+# Funktion um JSON Datei zu lesen.
 def GetJsonData(dataFilePath):
     with open(dataFilePath,'r') as jsonFile:
         jsonData = json.load(jsonFile)
         return jsonData
 
+# Programm Einstiegspunkt
 def main():
     ReadUnityData('20testX.json')
 
