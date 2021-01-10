a0a24dd9c9c4601ee2be469001b5d128d4c330f1
Jan Feldmann <janlukasfeldmann@gmail.com>
2021-01-10 14:42:29 +0100

    Added Custom Inspector with Button for StartValueRead() to ValuePrinter.

--- Bachelor-Unity/Assets/Scripts/ValuePrinter.cs
+++ Bachelor-Unity/Assets/Scripts/ValuePrinter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 /// <summary>
@@ -92,6 +93,25 @@ public class ValuePrinter : MonoBehaviour
             readDuration = _duration;
             readFrequenzy = _freq;
         }
+    }
+}
+
+/// <summary>
+/// Editor Klasse, fügt dem Inspector in Unity einen Button hinzu, mit dem man manuell
+/// das Starten des Lesevorgangs initialisieren kann.
+/// </summary>
+[CustomEditor(typeof(ValuePrinter))]
+public class ValuePrinterEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        ValuePrinter script = (ValuePrinter) target;
+        if (GUILayout.Button("Start Reading"))
+        {
+            script.StartValueRead();
+        }
+
     }
 }
 
