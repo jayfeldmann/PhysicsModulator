1896049e8aff7c41a232d269e576affad303e871
Jan Feldmann <janlukasfeldmann@gmail.com>
2021-01-26 20:35:04 +0100

    Commented DataHandler class

--- Unity Project/Assets/Scripts/DataHandler.cs
+++ Unity Project/Assets/Scripts/DataHandler.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Basisklasse für Midi und OSC cotroller.
+/// </summary>
 public class DataHandler : MonoBehaviour
 {
     protected SendController _sendController;
