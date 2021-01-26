bfd8066bae58c1f7648a6b6b910ddfffc5934ec3
Jan Feldmann <janlukasfeldmann@gmail.com>
2021-01-26 19:54:31 +0100

    Commented GameSettingsSaveData class

--- Unity Project/Assets/Scripts/Serialization/GameSettingsSaveData.cs
+++ Unity Project/Assets/Scripts/Serialization/GameSettingsSaveData.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Datenstruktur zum Speichern der Generellen Einstellungen wie Midigeräte und Netzwerkeinstellungen
+/// </summary>
 public class GameSettingsSaveData
 {
     public string midiDevice;
