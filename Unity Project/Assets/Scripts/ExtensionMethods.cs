81292db156fee2b8066e69db245d75ad314dc1ff
Jan Feldmann <janlukasfeldmann@gmail.com>
2021-01-26 20:38:32 +0100

    Commented ExtensionMethods class

--- Unity Project/Assets/Scripts/ExtensionMethods.cs
+++ Unity Project/Assets/Scripts/ExtensionMethods.cs
@@ -3,20 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class ExtensionMethods {
-    public static float Map (this float value, float from1, float to1, float from2, float to2) {
-        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
-    }
     
-    public static Vector2 RadianToVector2(float radian)
-    {
-        return new Vector2(Mathf.Cos(radian), Mathf.Sin(radian));
-    }
-      
-    public static Vector2 DegreeToVector2(float degree)
-    {
-        return RadianToVector2(degree * Mathf.Deg2Rad);
-    }
-    
+    // Funktion aus Unity Hilfsforum:
+    // https://answers.unity.com/questions/661383/whats-the-most-efficient-way-to-rotate-a-vector2-o.html
     public static Vector2 Rotate(Vector2 v, float degrees) {
         float sin = Mathf.Sin(degrees * Mathf.Deg2Rad);
         float cos = Mathf.Cos(degrees * Mathf.Deg2Rad);
